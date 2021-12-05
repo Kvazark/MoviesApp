9e641db1668d8d4d963347269028257e42427611
first <Katya_Klebleeva@mail.ru>
2021-12-05 10:44:57 +0300

    Add midlleware for the actor

---
+++ MoviesApp/Middleware/RequestLogMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace MoviesApp.Middleware
+{
+    public class RequestLogMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<RequestLogMiddleware> _logger;
+
+        public RequestLogMiddleware(RequestDelegate next,ILoggerFactory loggerFac)
+        {
+            _next = next;
+            _logger = loggerFac.CreateLogger<RequestLogMiddleware>();
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext.Request.Path.Value.ToLower().Contains("/actors"))
+            {
+                _logger.LogTrace($"Actor request: {httpContext.Request.Path.Value}  Method: {httpContext.Request.Method}");
+            }
+            await _next(httpContext);
+
+        }
+    }
+}
--- MoviesApp/Startup.cs
+++ MoviesApp/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MoviesApp.Data;
+using MoviesApp.Middleware;
 
 namespace MoviesApp
 {
@@ -40,7 +41,13 @@ namespace MoviesApp
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseActorControllerLogger();
             }
+            else    
+            {
+                app.UseExceptionHandler("/Error");     
+                app.UseHsts();    
+            }    
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
